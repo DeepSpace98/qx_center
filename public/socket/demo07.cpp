85ca5c35c87be7276b52b664e2962ea20231d972
liy <1247541226@qq.com>
2023-04-21 18:34:48 +0800

    tcp socket

--- public/socket/demo07.cpp
+++ public/socket/demo07.cpp
@@ -1,6 +1,6 @@
 /*
  * 程序名：demo07.cpp，此程序用于演示采用TcpClient类实现socket通讯的客户端。
- * 作者：吴从周。
+ * 作者：Liy。
 */
 #include "../_public.h"
  
@@ -22,7 +22,7 @@ int main(int argc,char *argv[])
   char buffer[102400];
  
   // 与服务端通讯，发送一个报文后等待回复，然后再发下一个报文。
-  for (int ii=0;ii<100000;ii++)
+  for (int ii=0;ii<1000;ii++)
   {
     SPRINTF(buffer,sizeof(buffer),"这是第%d个超级女生，编号%03d。",ii+1,ii+1);
     if (TcpClient.Write(buffer)==false) break; // 向服务端发送请求报文。

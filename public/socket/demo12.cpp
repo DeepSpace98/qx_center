89b4dc68dda64f967b28e9f4cf97b1230df3e7fa
liy <1247541226@qq.com>
2023-04-26 19:09:13 +0800

    tcp heartbeat

--- public/socket/demo12.cpp
+++ public/socket/demo12.cpp
@@ -1,29 +1,21 @@
 /*
- * 程序名：demo12.cpp，此程序用于演示网银APP软件的服务端。
- * 作者：吴从周
+ * 程序名：demo12.cpp，此程序演示网银APP的服务端。
+ *
+ * 作者：Liy
 */
 #include "../_public.h"
 
 CLogFile logfile;      // 服务程序的运行日志。
 CTcpServer TcpServer;  // 创建服务端对象。
 
+bool bsession = false; // 用户是否登录成功
+
 void FathEXIT(int sig);  // 父进程退出函数。
 void ChldEXIT(int sig);  // 子进程退出函数。
+bool _main(const char *strrecvbuffer, char *strsendbuffer); // 业务处理的主函数。
+bool srv001(const char *strrecvbuffer, char *strsendbuffer);
+bool srv002(const char *strrecvbuffer, char *strsendbuffer);
 
-bool bsession=false;     // 客户端是否已登录：true-已登录;false-未登录或登录失败。
-
-// 处理业务的主函数。
-bool _main(const char *strrecvbuffer,char *strsendbuffer);
-
-// 登录业务处理函数。
-bool srv001(const char *strrecvbuffer,char *strsendbuffer);
-
-// 查询余额业务处理函数。
-bool srv002(const char *strrecvbuffer,char *strsendbuffer);
-
-// 转账。
-bool srv003(const char *strrecvbuffer,char *strsendbuffer);
- 
 int main(int argc,char *argv[])
 {
   if (argc!=3)
@@ -66,15 +58,15 @@ int main(int argc,char *argv[])
 
     // 与客户端通讯，接收客户端发过来的报文后，回复ok。
     while (1)
-    {
+    { 
+
       memset(strrecvbuffer,0,sizeof(strrecvbuffer));
       memset(strsendbuffer,0,sizeof(strsendbuffer));
-
       if (TcpServer.Read(strrecvbuffer)==false) break; // 接收客户端的请求报文。
       logfile.Write("接收：%s\n",strrecvbuffer);
 
-      // 处理业务的主函数。
-      if (_main(strrecvbuffer,strsendbuffer)==false) break;
+      //业务处理的主函数
+      if(_main(strrecvbuffer, strsendbuffer) == false) break;
 
       if (TcpServer.Write(strsendbuffer)==false) break; // 向客户端发送响应结果。
       logfile.Write("发送：%s\n",strsendbuffer);
@@ -112,82 +104,61 @@ void ChldEXIT(int sig)
   exit(0);
 }
 
-// 处理业务的主函数。
-bool _main(const char *strrecvbuffer,char *strsendbuffer)
-{
-  // 解析strrecvbuffer，获取服务代码（业务代码）。
-  int isrvcode=-1;
-  GetXMLBuffer(strrecvbuffer,"srvcode",&isrvcode);
+// 业务处理的主函数
+bool _main(const char *strrecvbuffer, char *strsendbuffer){
+  // 解析strrecvbuffer，获取服务代码。
+  int isrvcode = -1;
+  GetXMLBuffer(strrecvbuffer, "srvcode", &isrvcode);
 
-  if ( (isrvcode!=1) && (bsession==false) )
-  {
-    strcpy(strsendbuffer,"<retcode>-1</retcode><message>用户未登录。</message>"); return true;
+  if((isrvcode != 1) && (bsession == false)){
+    strcpy(strsendbuffer, " <retcode>-1</retcode><message>用户未登录。</message>");
+    return true;
   }
-
   // 处理每种业务。
-  switch (isrvcode)
-  {
+  switch(isrvcode){
     case 1:   // 登录。
-      srv001(strrecvbuffer,strsendbuffer); break;
+      srv001(strrecvbuffer, strsendbuffer);
+      break;
     case 2:   // 查询余额。
-      srv002(strrecvbuffer,strsendbuffer); break;
-    case 3:   // 转账。
-      srv003(strrecvbuffer,strsendbuffer); break;
+      srv002(strrecvbuffer, strsendbuffer);
+      break;
     default:
-      logfile.Write("业务代码不合法：%s\n",strrecvbuffer); return false;
+      logfile.Write("业务代码不合法： %s \n", strrecvbuffer);
+      return false;
   }
-
   return true;
 }
 
-// 登录。
-bool srv001(const char *strrecvbuffer,char *strsendbuffer)
-{
-  // <srvcode>1</srvcode><tel>1392220000</tel><password>123456</password>
-
+// 登录
+bool srv001(const char *strrecvbuffer, char *strsendbuffer){
   // 解析strrecvbuffer，获取业务参数。
-  char tel[21],password[31];
-  GetXMLBuffer(strrecvbuffer,"tel",tel,20);
-  GetXMLBuffer(strrecvbuffer,"password",password,30);
+  char tel[21], password[31];
+  GetXMLBuffer(strrecvbuffer, "tel", tel, 20);
+  GetXMLBuffer(strrecvbuffer, "password", password, 30);
 
-  // 处理业务。
-  // 把处理结果生成strsendbuffer。
-  if ( (strcmp(tel,"1392220000")==0) && (strcmp(password,"123456")==0) )
-  {
-    strcpy(strsendbuffer,"<retcode>0</retcode><message>成功。</message>");  bsession=true;
+  // 处理业务
+  //把处理结果生成strsendbuffer
+  if ((strcmp(tel, "1392220000") == 0) && (strcmp(password, "123456") == 0)){
+    strcpy(strsendbuffer, "<retcode>0</retcode><message>成功。</message>");
+    bsession = true;
+  } else {
+    strcpy(strsendbuffer, "<retcode>-1</retcode><message>失败。</message>");
   }
-  else
-    strcpy(strsendbuffer,"<retcode>-1</retcode><message>失败。</message>");
-
   return true;
 }
 
-// 查询余额业务处理函数。
-bool srv002(const char *strrecvbuffer,char *strsendbuffer)
-{
-  // <srvcode>2</srvcode><cardid>62620000000001</cardid>
-
+// 查询余额
+bool srv002(const char *strrecvbuffer, char *strsendbuffer){
   // 解析strrecvbuffer，获取业务参数。
   char cardid[31];
-  GetXMLBuffer(strrecvbuffer,"cardid",cardid,30);
+  GetXMLBuffer(strrecvbuffer, "cardid", cardid, 30);
 
-  // 处理业务。
-  // 把处理结果生成strsendbuffer。
-  if (strcmp(cardid,"62620000000001")==0) 
-    strcpy(strsendbuffer,"<retcode>0</retcode><message>成功。</message><ye>100.58</ye>");
-  else
-    strcpy(strsendbuffer,"<retcode>-1</retcode><message>失败。</message>");
-
+  // 处理业务
+  //把处理结果生成strsendbuffer
+  if ((strcmp(cardid, "62620000000001") == 0)){
+    strcpy(strsendbuffer, "<retcode>0</retcode><message>成功。</message><ye>100.58</ye>");
+  } else {
+    strcpy(strsendbuffer, "<retcode>-1</retcode><message>失败。</message>");
+  }
   return true;
 }
-
-// 转账。
-bool srv003(const char *strrecvbuffer,char *strsendbuffer)
-{
-  // 编写转账业务的代码。
-
-  strcpy(strsendbuffer,"<retcode>0</retcode><message>成功。</message><ye>100.58</ye>");
-
-  return true;
-}
-
